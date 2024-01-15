808bb362e0064646e225aa43cca3bcd3e981021b
eivindIsMyName <129282175+eivindIsMyName@users.noreply.github.com>
2024-01-16 00:36:36 +0100

    Udded hello world index.js

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ const add = require("../add");
 
 const router = new Router();
 
+router.get("/", (req, res) => {
+  res.json({
+    status: "Hello World!"
+  }); 
+});
+
 router.get("/sum/:number1/:number2", (req, res) => {
   const {number1, number2} = req.params;
   if(number1 == null || number2 == null) {
